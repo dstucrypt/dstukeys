af3a07b3494b7b4edd545fdd323b4abebea17c31
Ilya Petrov <ilya.muromec@gmail.com>
2014-05-28 10:31:32 +0300

    add new db config search path

--- dstu/app.py
+++ dstu/app.py
@@ -1,6 +1,7 @@
 import os.path
 from flask import Flask
 cfg_path = os.path.join(os.environ['HOME'], 'db.cfg')
+cfg_path = os.path.join(os.getcwd(), '..', 'db.cfg')
 
 app = Flask(__name__)
 app.config.from_pyfile(cfg_path, silent=True)
